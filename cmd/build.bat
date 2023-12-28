fef7f83fb44cf9c4ada8c6ff2e24aa6775ec183b
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-28 21:46:34 +0700

    Added colors to build.bat

--- cmd/build.bat
+++ cmd/build.bat
@@ -2,6 +2,18 @@
 
 mkdir build
 pushd build
+
 zig c++ -g ..\sources\main.cpp -o out.exe User32.lib
+if %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo Compilation [31mFailed[0m
+
+    popd
+    exit %ERRORLEVEL%
+)
+
+echo.
+echo Compilation [32mSucceeded[0m
+
 popd
 
