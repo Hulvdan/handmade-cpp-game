ab0cf53b253eb15de2de05a234072c1e895fa749
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-28 21:46:53 +0700

    A bit better setup.lua commands

--- .nvim-personal/setup.lua
+++ .nvim-personal/setup.lua
@@ -1,12 +1,27 @@
-vim.keymap.set("n", "\\b", function() 
-    vim.fn.execute('term cmd\\build.bat')
-end)
+local opts = { remap = false, silent = true }
 
-vim.keymap.set("n", "<f5>", function() 
-    vim.fn.execute('term cmd\\build.bat && cmd\\run.bat')
-end)
+vim.keymap.set("n", "\\b", function()
+    vim.fn.execute("vs +term\\ cmd\\\\build.bat")
+    vim.fn.execute("wincmd x")
+    vim.fn.execute("wincmd l")
+end, opts)
 
-vim.keymap.set("n", "<f6>", function() 
-    vim.fn.execute('term cmd\\build.bat && cmd\\debug.bat')
-end)
+vim.keymap.set("n", "<f5>", function()
+    vim.fn.execute("wincmd n term\\ cmd\\build.bat\\ &&\\ cmd\\run.bat Build")
+end, opts)
+
+vim.keymap.set("n", "<f6>", function()
+    vim.fn.execute("term cmd\\build.bat && cmd\\debug.bat")
+end, opts)
+
+vim.keymap.set("n", "<C-\\>", function()
+    vim.fn.execute("vs")
+end, opts)
+vim.keymap.set("n", "<C-S-\\>", function()
+    vim.fn.execute("vs +echo 1")
+    vim.fn.execute("wincmd l")
+end, opts)
+
+vim.fn.execute(":set nornu")
+vim.fn.execute(":set nonumber")
 
