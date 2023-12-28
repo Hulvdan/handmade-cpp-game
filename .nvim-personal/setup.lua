3eb9773b730a41fd27f8da1c6f18896e3a3d4094
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-29 00:43:50 +0700

    Saving file before building / running

--- .nvim-personal/setup.lua
+++ .nvim-personal/setup.lua
@@ -7,16 +7,21 @@ vim.fn.execute(":set nonumber")
 -- ================== --
 local opts = { remap = false, silent = true }
 vim.keymap.set("n", "\\b", function()
+    vim.fn.execute(":w")
     vim.fn.execute("vs +term\\ cmd\\\\build.bat")
     vim.fn.execute("wincmd x")
     vim.fn.execute("wincmd l")
 end, opts)
 
 vim.keymap.set("n", "<f5>", function()
-    vim.fn.execute("term cmd\\build.bat && cmd\\run.bat")
+    vim.fn.execute(":w")
+    vim.fn.execute("vs +term\\ cmd\\\\build.bat && cmd\\run.bat")
+    vim.fn.execute("wincmd x")
+    vim.fn.execute("wincmd l")
 end, opts)
 
 vim.keymap.set("n", "<f6>", function()
+    vim.fn.execute(":w")
     vim.fn.execute("term cmd\\build.bat && cmd\\debug.bat")
 end, opts)
 
