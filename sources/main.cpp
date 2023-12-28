a62202b4aa3a51d23af118858103e4315cc18aa3
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-28 23:00:37 +0700

    Displayed the window with minimize, maximize and close buttons

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,15 +3,32 @@
 
 const auto BFG_CLASS_NAME = "BFGWindowClass";
 
+
 LRESULT windowEventsHandler(
-    HWND hInstance,
-    UINT    message_type,
-    WPARAM  wParam,
-    LPARAM  lParam
+    HWND   hInstance,
+    UINT   message_type,
+    WPARAM wParam,
+    LPARAM lParam
 ) {
+    switch (message_type) {
+        case (WM_CLOSE): {
+            return DefWindowProc(hInstance, message_type, wParam, lParam);
+        } break;
+
+        case (WM_SIZE): {
+        } break;
+
+        // case WM_SIZE: {
+        // } break;
+        //
+        // case WM_SIZE: {
+        // } break;
+
+        default:
+            return DefWindowProc(hInstance, message_type, wParam, lParam);
+    }
     return 0;
 }
-
 
 int WinMain(
     HINSTANCE hInstance,
@@ -19,10 +36,10 @@ int WinMain(
     LPSTR     lpCmdLine,
     int       nShowCmd
 ) {
-
-    WNDCLASSA wc = { };
+    WNDCLASSA wc = {};
     wc.style = CS_OWNDC; // UINT      style;
     wc.lpfnWndProc = *windowEventsHandler; // WNDPROC   lpfnWndProc;
+    wc.lpszClassName = BFG_CLASS_NAME;
     // int       cbClsExtra;
     // int       cbWndExtra;
     wc.hInstance = hInstance; //  HINSTANCE hInstance;
@@ -38,7 +55,7 @@ int WinMain(
         0,
         BFG_CLASS_NAME,
         "The Big Fuken Game",
-        WS_VISIBLE | WS_BORDER, // TODO(Hulvdan): WS_MAXIMIZE, WS_MAXIMIZEBOX, WS_MINIMIZE, WS_MINIMIZE_BOX
+        WS_TILEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
         NULL,       // [in, optional] HWND      hWndParent,
         NULL,       // [in, optional] HMENU     hMenu,
@@ -51,6 +68,12 @@ int WinMain(
     }
 
     ShowWindow(window_handle, nShowCmd);
+
+    MSG message = {};
+    while (GetMessage(&message, NULL, 0, 0) > 0) {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
 
     return 0;
 }
