69c3692957a1bf7c0166577d4e5d6210f887a6a8
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-30 12:20:12 +0700

    chore: nvim...

--- .nvim-personal/setup.lua
+++ .nvim-personal/setup.lua
@@ -2,26 +2,27 @@
 -- ================== --
 vim.fn.execute(":set nornu")
 vim.fn.execute(":set nonumber")
+vim.fn.execute(":set signcolumn=no")
 
 -- Keyboard Shortcuts --
 -- ================== --
 local opts = { remap = false, silent = true }
 vim.keymap.set("n", "\\b", function()
-    vim.fn.execute(":w")
+    -- vim.fn.execute(":w")
     vim.fn.execute("vs +term\\ cmd\\\\build.bat")
     vim.fn.execute("wincmd x")
     vim.fn.execute("wincmd l")
 end, opts)
 
 vim.keymap.set("n", "<f5>", function()
-    vim.fn.execute(":w")
+    -- vim.fn.execute(":w")
     vim.fn.execute("term cmd\\build.bat && cmd\\run.bat")
     vim.fn.execute("wincmd x")
     vim.fn.execute("wincmd l")
 end, opts)
 
 vim.keymap.set("n", "<f6>", function()
-    vim.fn.execute(":w")
+    -- vim.fn.execute(":w")
     vim.fn.execute("term cmd\\build.bat && cmd\\debug.bat")
 end, opts)
 
@@ -29,7 +30,6 @@ vim.keymap.set("n", "<C-\\>", function()
     vim.fn.execute("vs")
 end, opts)
 vim.keymap.set("n", "<C-S-\\>", function()
-    vim.fn.execute("vs +echo 1")
+    vim.fn.execute("vs")
     vim.fn.execute("wincmd l")
 end, opts)
-
