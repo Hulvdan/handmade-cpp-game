8ac3b8f4bc7198ee80c0a6c9f0d81b703089587c
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-05-23 00:58:15 +0500

    nvim: tweaks

--- .nvim-personal/callbacks.lua
+++ .nvim-personal/callbacks.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_create_autocmd({"BufEnter"}, {
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
     pattern = "*",
     once = false,
     callback = function()
@@ -10,5 +10,5 @@ vim.api.nvim_create_autocmd({"BufEnter"}, {
         if vim.bo.filetype ~= nil and vim.bo.filetype ~= "" then
             vim.fn.execute("set nowrap")
         end
-    end
+    end,
 })
--- .nvim-personal/setup.lua
+++ .nvim-personal/setup.lua
@@ -10,9 +10,10 @@ vim.fn.execute(":set nowritebackup")
 -- Helper Functions --
 -- ================ --
 local overseer = require("overseer")
+local formatter_util  = require("formatter.util")
 
 function launch_background(command, callback)
-    vim.fn.jobstart(command, {on_exit = callback})
+    vim.fn.jobstart(command, { on_exit = callback })
 end
 
 function save_files()
@@ -24,18 +25,21 @@ function reload_file()
 end
 
 function overseer_run(cmd)
-    overseer.new_task({
-        cmd = cmd,
-        components = {
-            { "on_output_quickfix", open = true, close = true },
-            { "on_exit_set_status", success_codes = { 0 } },
-            "default",
-        },
-    }):start()
+    save_files()
+    overseer
+        .new_task({
+            cmd = cmd,
+            components = {
+                { "on_output_quickfix", open = true, close = true },
+                { "on_exit_set_status", success_codes = { 0 } },
+                "default",
+            },
+        })
+        :start()
 end
 
 function cli_command(cmd)
-    return [[poetry run python cmd\cli.py ]] .. cmd
+    return [[.venv\Scripts\python.exe -OO cmd\cli.py ]] .. cmd
 end
 
 -- Keyboard Shortcuts --
@@ -43,45 +47,41 @@ end
 local opts = { remap = false, silent = true }
 
 vim.keymap.set("n", "<leader>l", function()
-    save_files()
     overseer_run(cli_command("lint"))
 end, opts)
 
 vim.keymap.set("n", "<A-b>", function()
-    save_files()
     overseer_run(cli_command("build"))
 end, opts)
 
 vim.keymap.set("n", "<A-g>", function()
-    save_files()
     overseer_run(cli_command("generate"))
 end, opts)
 
 vim.keymap.set("n", "<C-S-b>", function()
-    save_files()
     overseer_run(cli_command("cmake_vs_files"))
 end, opts)
 
 vim.keymap.set("n", "<f5>", function()
-    save_files()
     overseer_run(cli_command("stoopid_windows_visual_studio_run"))
 end, opts)
 
 vim.keymap.set("n", "<A-t>", function()
-    save_files()
     overseer_run(cli_command("test"))
 end, opts)
 
 vim.keymap.set("n", "<leader>w", function()
     save_files()
 
+    -- TODO: Переписать на formatter.nvim?
     if vim.bo.filetype == "cpp" or vim.bo.filetype == "h" then
         local view = vim.fn.winsaveview()
         local buf_path = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
 
-        -- TODO: Форматировать активный файл.
-        -- OLD REF: launch_background([[cmd\format.bat "]] .. buf_path .. '"', function()
-        launch_background(cli_command("format"), function()
+        -- NOTE: Python's startup time is so fuken slow it's barely usable
+        -- local cmd = cli_command("format " .. formatter_util.escape_path(buf_path))
+        local cmd = '"C:/Program Files/LLVM/bin/clang-format.exe" -i ' .. formatter_util.escape_path(buf_path)
+        launch_background(cmd, function()
             reload_file()
             vim.fn.winrestview(view)
             -- NOTE: костыль для нормального отображения nvim-treesitter-context
